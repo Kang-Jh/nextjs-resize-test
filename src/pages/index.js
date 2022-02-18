ec813124a50f9081e3e68b7b41a91f3f9060a415
Kang-Jh <iamkangjh@gmail.com>
2022-02-18 18:26:14 +0900

    이미지 리사이즈 테스트

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import image01 from '../images/01.jpg';
+import image02 from '../images/02.jpg';
+import image03 from '../images/03.jpg';
+import image04 from '../images/04.jpg';
+import image05 from '../images/05.jpg';
+import image06 from '../images/06.jpg';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Image src={image01} width="200" height="200" />
+      <Image src={image02} width="200" height="200" />
+      <Image src={image03} width="200" height="200" />
+      <Image src={image04} width="200" height="200" />
+      <Image src={image05} width="200" height="200" />
+      <Image src={image06} width="200" height="200" />
+    </div>
+  );
+}
